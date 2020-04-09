fe8f760d072e9312a15c97bbef9fc98724c712a8
James Gullickson <jgullick@umn.edu>
2020-04-08 19:48:16 -0500

    interop update

--- oauth.js
+++ oauth.js
@@ -4,4 +4,7 @@ if (window.interop){
     console.log(code)
     console.log('there is interop')
     window.interop.setAuthCode(code);
+} else {
+    console.log(code)
+    console.log('no interop')
 }
