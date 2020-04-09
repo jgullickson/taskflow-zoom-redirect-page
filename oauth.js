ab2e68a1e17a6e3ea3877c1726324f96b9a664c9
James Gullickson <jgullick@umn.edu>
2020-04-08 20:09:38 -0500

    setAuthCode

--- oauth.js
+++ oauth.js
@@ -1,11 +1,13 @@
 let code = window.location.search;
-console.log('udpated')
+console.log('udpated 2')
 
-if (window.interop){
-    console.log(code)
-    console.log('there is interop')
-    window.interop.setAuthCode(code);
-} else {
-    console.log(code)
-    console.log('no interop')
-}
+setAuthCode(code);
+
+// if (window.interop){
+//     console.log(code)
+//     console.log('there is interop')
+//     window.interop.setAuthCode(code);
+// } else {
+//     console.log(code)
+//     console.log('no interop')
+// }
